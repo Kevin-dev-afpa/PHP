f42d36626e5375f70f086c0554455718cc7ee917
Kevin-dev-afpa <kequintin@laposte.net>
2020-04-28 16:55:45 +0200

    Exercice sur la fonction calculatrice

--- bonjour.php
+++ bonjour.php
@@ -1,29 +1,39 @@
 <html>
- <?php 
+<head>
+       <title>Affichage du résultat en PHP</title>
+</head>
+ 
+<?php
 
-$a = array("19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
-           "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
-           "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
-     );
-//Exercice 0
-var_dump($a);     
-//Exercice 1
-echo 'La validation du groupe 19002 se déroulera la '.array_search('Validation', $a[19002])." eme semaines"."</br>";
-//Exercice 2
-echo 'Le stage se déroulera la : '.array_search('Stage', $a[19001]).' eme semaines'."</br>";
+function calculator($value1,$operator,$value2)
+{
+    if($operator === "+")
+    {
+        $resultat = $value1 + $value2;
+       echo $value1." + ". $value2 . "=" .  $resultat;
+    }
+    
+    if($operator === "-")
+    {
+        $resultat = $value1 - $value2;
+        echo $value1." - ". $value2 . "=" .  $resultat;
+    }
 
-$position = count($a[19001]);
-$stock=0;
-for($int = 0; $int < $position; $int++) {
-      $pos = strrpos($a[19001][$int], "Stage");
-            if ($pos === false) 
-            {
-            }
-            else {
-                  $stock = $int+1;
-            }
+    if($operator === "/")
+    {
+        $resultat = $value1 / $value2;
+        echo $value1." / ". $value2 . "=" .  $resultat;
+    }
+
+    if($operator === "*")
+    {
+        $resultat = $value1 * $value2;
+        echo $value1." * ". $value2 . "=" .  $resultat;
+    }
 }
-echo "La dernière semaine de stage sera la ".$stock." eme semaine";
 
-?> 
+calculator(2,"*",4); 
 
+?>
+</body>
+</html>
